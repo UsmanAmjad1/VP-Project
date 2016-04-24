f4eb51dd10015e20787b07891debef832aa10afc
Usman Amjad <usman.amjad60@yahoo.com>
2016-04-24 21:40:30 +0500

    work in saturday and combine two forms and improve design of guitar

---
+++ guitar/Form2.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace guitar
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape3 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape4 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape5 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape6 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape6,
+            this.lineShape5,
+            this.lineShape4,
+            this.lineShape3,
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(907, 325);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape1.X1 = 211;
+            this.lineShape1.X2 = 377;
+            this.lineShape1.Y1 = 124;
+            this.lineShape1.Y2 = 127;
+            this.lineShape1.Click += new System.EventHandler(this.lineShape1_Click);
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape2.X1 = 209;
+            this.lineShape2.X2 = 379;
+            this.lineShape2.Y1 = 132;
+            this.lineShape2.Y2 = 135;
+            this.lineShape2.Click += new System.EventHandler(this.lineShape2_Click);
+            // 
+            // lineShape3
+            // 
+            this.lineShape3.Name = "lineShape3";
+            this.lineShape3.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape3.X1 = 212;
+            this.lineShape3.X2 = 377;
+            this.lineShape3.Y1 = 141;
+            this.lineShape3.Y2 = 143;
+            // 
+            // lineShape4
+            // 
+            this.lineShape4.Name = "lineShape4";
+            this.lineShape4.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape4.X1 = 206;
+            this.lineShape4.X2 = 376;
+            this.lineShape4.Y1 = 149;
+            this.lineShape4.Y2 = 150;
+            // 
+            // lineShape5
+            // 
+            this.lineShape5.Name = "lineShape5";
+            this.lineShape5.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape5.X1 = 206;
+            this.lineShape5.X2 = 375;
+            this.lineShape5.Y1 = 158;
+            this.lineShape5.Y2 = 160;
+            // 
+            // lineShape6
+            // 
+            this.lineShape6.Name = "lineShape6";
+            this.lineShape6.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape6.X1 = 209;
+            this.lineShape6.X2 = 384;
+            this.lineShape6.Y1 = 165;
+            this.lineShape6.Y2 = 168;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.BackgroundImage = global::guitar.Properties.Resources._2761400_535092ba822e9099836cc2d144efb117;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(907, 325);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape3;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape6;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape5;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape4;
+    }
+}
---
+++ guitar/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace guitar
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void lineShape1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer mysound = new SoundPlayer(@"D:\VP-Project\guitar\Resources\b24.wav");
+            mysound.Play();
+        }
+
+        private void lineShape2_Click(object sender, EventArgs e)
+        {
+            SoundPlayer mysound = new SoundPlayer(@"D:\VP-Project\guitar\Resources\b24.wav");
+            mysound.Play();
+        }
+    }
+}
